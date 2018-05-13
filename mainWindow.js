ac6571395f4ed3376cbd7fe4b4caa97c5e7500ce
Michel Chieregato <michel.chieregato@gmail.com>
2018-05-13 18:07:25 -0300

    Primeiras implementações: Tela de Login e autenticação simples

---
+++ mainWindow.js
@@ -0,0 +1,45 @@
+const {BrowserWindow} = require('electron')
+
+const path = require('path')
+const url = require('url')
+
+exports.win 
+
+exports.createWindow = () => {
+    this.win = new BrowserWindow({
+        // useContentSize: true,
+        width: 1000,
+        height: 800,
+        minWidth: 350,
+        minHeight: 300,
+        autoHideMenuBar: true
+    })
+
+    this.win.webContents.on('did-finish-load', () => {
+        this.win.webContents.send('toLogin', "Hello, renderer.")
+    })
+
+
+    // DevTools
+    // this.win.webContents.openDevTools()
+
+    console.log(url.format({
+        pathname: path.join(__dirname, 'renderer/login.html'),
+        protocol: 'file:',
+        slashes: true
+      }))
+
+    // Load main window content
+    this.win.loadURL(url.format({
+        pathname: path.join(__dirname, 'src/html/login.html'),
+        protocol: 'file:',
+        slashes: true
+      }))
+
+    // Handling closing 
+
+    this.win.on('closed', () => {
+        this.win = null
+    })
+
+}
