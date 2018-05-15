032e547d080d8592001500057dd51e1a0f221354
gkhonda <gustavokenhonda@gmail.com>
2018-05-15 00:28:45 -0300

    fix

--- mainWindow.js
+++ mainWindow.js
@@ -3,43 +3,43 @@ const {BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
 
-exports.win 
+exports.win
 
 exports.createWindow = () => {
-    this.win = new BrowserWindow({
-        // useContentSize: true,
-        width: 1000,
-        height: 800,
-        minWidth: 350,
-        minHeight: 300,
-        autoHideMenuBar: true
-    })
+  this.win = new BrowserWindow({
+    // useContentSize: true,
+    width: 1000,
+    height: 800,
+    minWidth: 350,
+    minHeight: 300,
+    autoHideMenuBar: true
+  })
 
-    this.win.webContents.on('did-finish-load', () => {
-        this.win.webContents.send('toLogin', "Hello, renderer.")
-    })
+  this.win.webContents.on('did-finish-load', () => {
+    this.win.webContents.send('toLogin', "Hello, renderer.")
+  })
 
 
-    // DevTools
-    // this.win.webContents.openDevTools()
+  // DevTools
+  // this.win.webContents.openDevTools()
 
-    console.log(url.format({
-        pathname: path.join(__dirname, 'renderer/login.html'),
-        protocol: 'file:',
-        slashes: true
-      }))
+  console.log(url.format({
+    pathname: path.join(__dirname, 'renderer/login.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
 
-    // Load main window content
-    this.win.loadURL(url.format({
-        pathname: path.join(__dirname, 'src/html/login.html'),
-        protocol: 'file:',
-        slashes: true
-      }))
+  // Load main window content
+  this.win.loadURL(url.format({
+    pathname: path.join(__dirname, 'src/html/login.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
 
-    // Handling closing 
+  // Handling closing
 
-    this.win.on('closed', () => {
-        this.win = null
-    })
+  this.win.on('closed', () => {
+    this.win = null
+  })
 
 }
--- src/js/login.js
+++ src/js/login.js
@@ -17,49 +17,42 @@ const url = require('url')
 
 let win
 
-const {ipcRenderer} = require('electron')
-
 // Send message to main process on channel login
 
 ipcRenderer.send('login', "Helloooooooooo main.js")
 
 // Expect the response from the main process
 ipcRenderer.on('toLogin', (e, args) => {
-    console.log(args)
+  console.log(args)
 })
 
 // Essa função é chamada ao clicar no botao
-$("#btnLogin").on("click", function (e) {
-
-    // Para manipular a Janela Atual
-    win = remote.getCurrentWindow()
-    // Cria um objeto com os dados do forms
-    var data = $('#form').serializeArray().reduce(function(obj, item) {
-        obj[item.name] = item.value;
-        return obj;
-    }, {});
-
-    // Cria um post request pro endereço (local), que ta rodando o meu Django
-    $.post( "http://127.0.0.1:8000/login/", data).done(function(back) {
-        console.log(back)
-        if(back['isAuth'] == false) {
-            console.log("Not Auth babyyyyyy")
-        } else if (back['Permission'] == "Administrador") {
-            win.loadURL((url.format({
-                    pathname: path.join(__dirname, '../html/menu.html'),
-                    protocol: 'file:',
-                    slashes: true
-                })))
-        } else {
-            win.loadURL((url.format({
-                pathname: path.join(__dirname, './login.html'),
-                protocol: 'file:',
-                slashes: true
-            })))
-        }
+$("#btnLogin").on("click", function (e)
+{
+  // Para manipular a Janela Atual
+  win = remote.getCurrentWindow()
+  // Cria um objeto com os dados do forms
+  var data = $('#form').serializeArray().reduce(function(obj, item) {
+    obj[item.name] = item.value;
+    return obj;
+  }, {});
+  // Cria um post request pro endereço (local), que ta rodando o meu Django
+  $.post( "http://127.0.0.1:8000/login/", data).done(function(back) {
+    console.log(back)
+    if(back['isAuth'] == false) {
+      console.log("Not Auth babyyyyyy")
+    } else if (back['Permission'] == "Administrador") {
+      win.loadURL((url.format({
+        pathname: path.join(__dirname, '../html/menu.html'),
+        protocol: 'file:',
+        slashes: true
+    })))
+    } else {
+      win.loadURL((url.format({
+        pathname: path.join(__dirname, '../html/login.html'),
+        protocol: 'file:',
+        slashes: true
+      })))
     }
-);
-
-
-
+  });
 })
