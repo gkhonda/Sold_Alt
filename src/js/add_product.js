50e67672dff5264aaab07232ed7dc21acf1442cb
chazanas <chazanaspln@gmail.com>
2018-06-05 03:55:00 -0300

    less lame but still lame

---
+++ src/js/add_product.js
@@ -0,0 +1,10 @@
+// alert('hi')
+
+// $('prices').keypress(function(event) {
+// 	if (event.which == 13) {
+// 		if (isNaN(Number($('this').val()))) {
+// 			$('this').val('');
+// 		}
+// 		console.log($('this').val())
+// 	}
+// })
