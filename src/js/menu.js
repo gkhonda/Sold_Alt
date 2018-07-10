398eed9353b8e319289f2248661d3763c223d05f
Michel Chieregato <michel.chieregato@gmail.com>
2018-07-10 14:50:02 -0300

    Boostrap should be version 3

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,3 +1,14 @@
 require('electron-window').parseArgs()
+const {BrowserWindow, getCurrentWindow} = require('electron').remote
+
+let win, new_win
+
+// Para manipular a Janela Atual
+win = getCurrentWindow()
 
 console.log(window.__args__)
+
+$('#sale').on('click', function(e) {
+    win.showUrl('src/html/sale.html', '', () => {
+      })
+})
