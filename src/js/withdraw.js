b717ccf053864aeb5fa8f65c3e81711c9cd67b79
Michel Chieregato <michel.chieregato@gmail.com>
2018-08-02 17:13:34 -0300

    Improvements on withdraw design

--- src/js/withdraw.js
+++ src/js/withdraw.js
@@ -9,18 +9,18 @@ $('#change-type').click(function () {
         $.post("http://127.0.0.1:8000/withdraw/read", {'method': 'Cheque'}).done(function (back) {
             console.log(back['quantity']);
             $('#money-quantity').text(back['quantity'].toFixed(2));
-            $('#span-method').text("Cheque");
+            $('.span-method').text("Cheque");
             type = "Cheque";
-            $("#type-image").attr("src", "../images/Dinheiro.png");
+            $("#type-text").val("DINHEIRO");
 
         });
     } else {
         $.post("http://127.0.0.1:8000/withdraw/read", {'method': 'Dinheiro'}).done(function (back) {
             console.log(back['quantity']);
             $('#money-quantity').text(back['quantity'].toFixed(2));
-            $('#span-method').text("Dinheiro");
+            $('.span-method').text("Dinheiro");
             type = "Dinheiro";
-            $("#type-image").attr("src", "../images/Cheque.png");
+            $("#type-text").val("CHEQUE");
 
         });
     }
