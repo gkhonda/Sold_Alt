ff1637427f39eb4f99329e4d497a209be9b87a06
Michel Chieregato <michel.chieregato@gmail.com>
2018-08-05 23:35:33 -0300

    Made Screen : Withdraw

--- src/js/withdraw.js
+++ src/js/withdraw.js
@@ -2,12 +2,21 @@ const remote = require('electron').remote;
 const {ipcRenderer} = require('electron');
 
 let type = "Dinheiro";
+
+// Formatar corretamente o valor para reais
+let valor;
+$('#name').on('keyup change', function (e) {
+    valor = formact(this, valor, e);
+});
+
+// Post request inicial para pegar a quantidade de dinheiro em caixa
 $.post("http://127.0.0.1:8000/withdraw/read", {'method': 'Dinheiro'}).done(function (back) {
     $('#money-quantity').text(back['quantity'].toFixed(2));
 });
 
-
+// Mudar dinheiro/cheque
 $('#change-type').click(function () {
+
     if (type === "Dinheiro") {
         $.post("http://127.0.0.1:8000/withdraw/read", {'method': 'Cheque'}).done(function (back) {
             console.log(back['quantity']);
@@ -30,11 +39,61 @@ $('#change-type').click(function () {
 
 });
 
+// Fazer retirada de dinheiro
 $('#make-withdraw').on('click', function () {
+    let qnt = parseFloat(valor) / 100
+    console.log(valor);
+    console.log(parseFloat(valor));
+    console.log(parseFloat($('#money-quantity').text()))
 
-    let qnt = parseFloat($('.form-control').val());
+    if (parseFloat(qnt) > parseFloat($('#money-quantity').text())) {
+        ipcRenderer.send('login', {
+            'type': 'ok-face',
+            'message': 'Operação não permitida.',
+            'text': "Não se pode retirar um valor maior do que a quantidade atual."
+        });
+    } else {
+        update_withdraw_database(1);
+    }
+});
+
+// Fazer depósito de dinheiro
+$('#deposit').on('click', function () {
+    update_withdraw_database(0);
+});
+
+function formact(element, valor, event) {
+
+    let formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'BRL',
+        minimumFractionDigits: 2,
+    });
+
+    if (event.which < 58 && event.which > 47) {
+        last_typo = $(element).val()[$(element).val().length - 1];
+        if (valor === undefined) {
+            valor = $(element).val()
+        } else {
+            valor = valor + last_typo;
+        }
+    } else if (event.which === 8) {
+        valor = $(element).val().replace(/\D/g, "");
+    }
+
+    if (valor.length > 0) {
+        $(element).val((formatter.format((parseFloat(valor) / 100).toFixed(2))));
+    } else {
+        $(element).val(formatter.format(0));
+    }
+
+
+    return valor;
+}
+
+var update_withdraw_database = function (is_withdraw) {
+    let qnt = (parseFloat(valor) / 100).toFixed(2);
     let user_id = remote.getGlobal('Vendedor_id');
-    let is_withdraw = true;
 
     let args = {
         'type': type,
@@ -43,8 +102,18 @@ $('#make-withdraw').on('click', function () {
         'withdraw': is_withdraw
     };
 
+
+    if (valor == 0 || valor === undefined || isNaN(valor)) {
+        ipcRenderer.send('login', {
+            'type': 'ok-face',
+            'message': 'Preencha corretamente os campos.',
+            'text': "Não esqueça de adicionar um valor não nulo."
+        });
+        return;
+    }
+
     $.post("http://127.0.0.1:8000/withdraw/create", args).done(function (back) {
-        if (back['Error'] === true) {
+        if (back['Error'] === 1) {
             ipcRenderer.send('login',
                 {
                     'type': 'sad',
@@ -57,8 +126,9 @@ $('#make-withdraw').on('click', function () {
                 {
                     'type': 'happy',
                     'message': 'Sucesso!',
-                    'text': 'Retirada de dinheiro feita com sucesso cadastrada!'
-                })
+                    'text': 'Operação feita com sucesso!'
+                });
+            location.reload();
         }
     }).fail(function () {
         ipcRenderer.send('login',
@@ -68,5 +138,4 @@ $('#make-withdraw').on('click', function () {
                 'text': 'Verifique a conexão'
             })
     });
-
-});
+};
