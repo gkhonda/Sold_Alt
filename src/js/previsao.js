825b5c8492d5d4ee35642f33cdd88d4a28174681
chazanas <chazanaspln@gmail.com>
2018-10-07 22:28:33 -0300

    integration done

--- src/js/previsao.js
+++ src/js/previsao.js
@@ -7,30 +7,31 @@ $(function () {
 });
 
 
-var month = {
-    0: 'janeiro',
-    1: 'fevereiro',
-    2: 'março',
-    3: 'abril',
-    4: 'maio',
-    5: 'junho',
-    6: 'julho',
-    7: 'agosto',
-    8: 'setembro',
-    9: 'outubro',
-    10: 'novembro',
-    11: 'dezembro'
+var months = {
+    1: 'janeiro',
+    2: 'fevereiro',
+    3: 'março',
+    4: 'abril',
+    5: 'maio',
+    6: 'junho',
+    7: 'julho',
+    8: 'agosto',
+    9: 'setembro',
+    10: 'outubro',
+    11: 'novembro',
+    12: 'dezembro'
 }
 var date = new Date();
-date = date.getMonth();
-$('.mes').text(month[date + 1]);
+var month = date.getMonth() + 2;
+var year = date.getFullYear();
+$('.mes').text(months[month]);
 
 $('.generate').on('click', function(){
-    getPredictions({'create': true})
+    getPredictions({'create': true, 'month': month, 'year': year})
 });
 
 $('.open').on('click', function() {
-    getPredictions({'create': false})
+    getPredictions({'create': false, 'month': month, 'year': year})
 });
 
 function getPredictions(request){
